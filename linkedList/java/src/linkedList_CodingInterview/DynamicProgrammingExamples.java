c89411239892873ff92910bb69fd41108836810d
Dylan Thiemann <dylanthiemann@gmail.com>
2014-10-29 14:10:28 -0500

    Adding more practice code

--- linkedList/java/src/linkedList_CodingInterview/DynamicProgrammingExamples.java
+++ linkedList/java/src/linkedList_CodingInterview/DynamicProgrammingExamples.java
@@ -26,25 +26,27 @@ public class DynamicProgrammingExamples {
 			return 0;
 		}
 		
-		if (steps == 0) { 
+		else if (steps == 0) { 
 			return 1;
 		}
 		
-		if (map[steps] > -1)
+		else if (map[steps] > -1)
 			return map[steps];
-
-		return map[steps] + numberOfDifferentTraversals(steps - 3) + 
-				numberOfDifferentTraversals(steps - 2) + 
-				numberOfDifferentTraversals(steps - 1);
+		else {
+			map[steps] = numberOfDifferentTraversalsDP(steps - 3, map) + 
+					numberOfDifferentTraversalsDP(steps - 2, map) + 
+					numberOfDifferentTraversalsDP(steps - 1, map);
+			return map[steps];
+		}
 	}
 	
 	public static void helper() {
 		System.out.println(numberOfDifferentTraversals(15));
 		
-		int n = 15;
-		int[] map = new int[n];
-		for (int i = 0; i < n; i++) {
-			map[0] = -1;
+		int n = 5;
+		int[] map = new int[n+1];
+		for (int i = 0; i < n+1; i++) {
+			map[i] = -1;
 		}
 		System.out.println(numberOfDifferentTraversalsDP(n, map));
 	}
