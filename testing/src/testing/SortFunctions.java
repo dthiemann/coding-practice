77e7b0e8f9d953e99a1a89c33cdce6bc0413d0a0
Dylan Thiemann <dylanthiemann@gmail.com>
2014-11-07 08:58:00 -0600

    Added basic sorting algorithms (in Java)

---
+++ testing/src/testing/SortFunctions.java
@@ -0,0 +1,68 @@
+package testing;
+
+public class SortFunctions {
+
+	public static void selectionSort(int[] myArray) {
+		for (int i=0; i < myArray.length; i++) {
+			int currentMin = i;
+			for (int j=i; j < myArray.length; j++) {
+				if (myArray[currentMin] > myArray[j]) {
+					currentMin = j;
+				}
+			}
+			if (currentMin != i) {
+				int temp = myArray[i];
+				myArray[i] = myArray[currentMin];
+				myArray[currentMin] = temp;
+			}
+		}
+	}
+	
+	public static void insertionSort(int[] myArray) {
+		for (int i=0; i < myArray.length; i++) {
+			int j = i;
+			int currentValue = myArray[i];
+			while (j > 0 && myArray[j-1] >= currentValue) {
+				myArray[j] = myArray[j-1];
+				j--;
+			}
+			myArray[j] = currentValue;
+		}
+	}
+	
+	public static void bubbleSort(int[] myArray) {
+		for (int i = 0; i < myArray.length; i++) {
+			for (int j = 0; j < (myArray.length - i); j++) {
+				if (myArray[i] > myArray[j]) {
+					int temp = myArray[i];
+					myArray[i] = myArray[j];
+					myArray[j] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		int[] myArray = {4,3,7,3,4,2,7,9,4,4,2};
+		selectionSort(myArray);
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.print(myArray[i]);
+			System.out.print(", ");
+		}
+		System.out.println();
+		int[] myArray2 = {5,2,7,3,5,2,5,2,7,2,8};
+		insertionSort(myArray2);
+		for (int i = 0; i < myArray2.length; i++) {
+			System.out.print(myArray2[i]);
+			System.out.print(", ");
+		}
+		System.out.println();
+		int[] myArray3 = {5,2,7,3,5,2,5,2,7,2,8};
+		insertionSort(myArray3);
+		for (int i = 0; i < myArray3.length; i++) {
+			System.out.print(myArray3[i]);
+			System.out.print(", ");
+		}
+	}
+	
+}
